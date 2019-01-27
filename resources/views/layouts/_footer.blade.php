70a14cfb09cf524d51f3fb18659267a30932d95f
wvxiaobai <wvxiaobai@gmail.com>
2019-01-27 11:35:13 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/images/201802/28/1/Jk8mC7SGI5.jpg!/both/200x200" alt="">
+    <a href="" target="_blank">
+        好好学习，天天向上
+    </a>
+
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
