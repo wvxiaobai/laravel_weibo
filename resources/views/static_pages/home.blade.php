5b17a89134514f679f384418a6a33ef9694d899d
wvxiaobai <wvxiaobai@gmail.com>
2019-01-27 11:05:40 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello laravel</h1>
+        <p class="lead">
+            你现在所看到的是
+        </p>
+        <p>
+            一切，将从这里开始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
